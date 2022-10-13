799a4d3575ad5fcd88216f665f72612e9ae22e9c
K Rain Leander <rainleander@users.noreply.github.com>
2022-10-13 11:43:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import random
 
-class Card(object):
+class Card( object ):
   def __init__(self, name, value, suit, symbol):
     self.value = value
     self.suit = suit
     self.name = name
     self.symbol = symbol
     self.showing = False
+
   def __repr__(self):
     if self.showing:
       return self.symbol
@@ -14,48 +15,42 @@ class Card(object):
       return "Card"
 
 class Deck(object):
-  def shuffle(self, times=1):
+  def shuffle(self, times=1 ):
     random.shuffle(self.cards)
     print("Deck Shuffled")
-  
+
   def deal(self):
     return self.cards.pop(0)
 
-class StandardDeck(object):
+class StandardDeck(Deck):
   def __init__(self):
     self.cards = []
     suits = {"Hearts":"♡", "Spades":"♠", "Diamonds":"♢", "Clubs":"♣"}
-    values = {"Two":2, 
-              "Three":3, 
-              "Four":4, 
-              "Five":5, 
-              "Six":6, 
-              "Seven":7, 
-              "Eight":8, 
-              "Nine":9, 
-              "Ten":10, 
-              "Jack":11, 
-              "Queen":12, 
-              "King":13, 
-              "Ace":14}
+    values = {"Two":2,
+              "Three":3,
+              "Four":4,
+              "Five":5,
+              "Six":6,
+              "Seven":7,
+              "Eight":8,
+              "Nine":9,
+              "Ten":10,
+              "Jack":11,
+              "Queen":12,
+              "King":13,
+              "Ace":14 }
+
     for name in values:
       for suit in suits:
         symbolIcon = suits[suit]
         if values[name] < 11:
           symbol = str(values[name])+symbolIcon
         else:
-          symbol = name[0] + symbolIcon
-        self.cards.append(Card(name, values[name], suit, symbol))
+          symbol = name[0]+symbolIcon
+        self.cards.append( Card(name, values[name], suit, symbol) )
 
   def __repr__(self):
-    return "Standard deck of cards:{0} cards remaining".format(len(self.cards))
-
-  def shuffle(self, times=1):
-    random.shuffle(self.cards)
-    print("Deck Shuffled")
-  
-  def deal(self):
-    return self.cards.pop(0)
+    return "Standard deck of cards:{0} remaining".format(len(self.cards))
 
 class Player(object):
   def __init__(self):
@@ -67,6 +62,7 @@ class Player(object):
   def addCard(self, card):
     self.cards.append(card)
 
+
 class PokerScorer(object):
   def __init__(self, cards):
     # Number of cards
@@ -77,26 +73,27 @@ class PokerScorer(object):
 
   def flush(self):
     suits = [card.suit for card in self.cards]
-    if len(set(suits)) == 1:
+    if len( set(suits) ) == 1:
       return True
-    else:
-      return False
+    return False
 
   def straight(self):
     values = [card.value for card in self.cards]
     values.sort()
 
-    if not len(set(values)) == 5:
-      return False
-    if value[4] == 14 and value[3] == 5 and value[2] == 4 and value[1] == 3 and value[0] == 2:
-      return True
-    else:
-      if not value[0] + 1 == value[1]: return False
-      if not value[1] + 1 == value[2]: return False
-      if not value[2] + 1 == value[3]: return False
-      if not value[3] + 1 == value[4]: return False
+    if not len( set(values)) == 5:
+      return False 
 
-    return True
+    if values[4] == 14 and values[0] == 2 and values[1] == 3 and values[2] == 4 and values[3] == 5:
+      return 5
+
+    else:
+      if not values[0] + 1 == values[1]: return False 
+      if not values[1] + 1 == values[2]: return False
+      if not values[2] + 1 == values[3]: return False
+      if not values[3] + 1 == values[4]: return False
+
+    return values[4]
 
   def highCard(self):
     values = [card.value for card in self.cards]
@@ -104,34 +101,170 @@ class PokerScorer(object):
     for card in self.cards:
       if highCard is None:
         highCard = card
-      elif highCard.value < card.value:
-        highCard = card
+      elif highCard.value < card.value: 
+        highCard=card
+
     return highCard
+
+  def highestCount(self):
+    count = 0
+    values = [card.value for card in self.cards]
+    for value in values:
+      if values.count(value) > count:
+        count = values.count(value)
+
+    return count
+
+  def pairs(self):
+    pairs = []
+    values = [card.value for card in self.cards]
+    for value in values:
+      if values.count(value) == 2 and value not in pairs:
+        pairs.append(value)
+
+    return pairs
+        
+  def fourKind(self):
+    values = [card.value for card in self.cards]
+    for value in values:
+      if values.count(value) == 4:
+        return True
+
+  def fullHouse(self):
+    two = False
+    three = False
+    
+    values = [card.value for card in self.cards]
+    if values.count(values) == 2:
+      two = True
+    elif values.count(values) == 3:
+      three = True
+
+    if two and three:
+      return True
+
+    return False
 
 def Poker():
   player = Player()
-  
-  # initial amount
-  initialAmount = 100
-  # cost per hand
+
+  # Intial Amount
+  points = 100
+
+  # Cost per hand
   handCost = 5
 
-  ## hand loop
-  discard = []
+  end = False
+  while not end:
+    print( "You have {0} points".format(points) )
+    print()
 
-  # shuffle
-  deck = StandardDeck()
-  deck.shuffle()
-  
-  # deal
-  for i in range(5):
-    player.addCard(deck.deal())
+    points-=5
 
-  for card in player.cards:
-    card.showing = True
-  
-  # hold or pass
-  # score
+    ## Hand Loop
+    deck = StandardDeck()
+    deck.shuffle()
 
-  print(player.cards)
-  
+    # Deal Out
+    for i in range(5):
+      player.addCard(deck.deal())
+
+    # Make them visible
+    for card in player.cards:
+      card.showing = True
+    print(player.cards)
+
+    validInput = False
+    while not validInput:
+      print("Which cards do you want to discard? ( ie. 1, 2, 3 )")
+      print("*Just hit return to hold all, type exit to quit")
+      inputStr = input()
+
+      if inputStr == "exit":
+        end=True
+        break
+
+      try:
+        inputList = [int(inp.strip()) for inp in inputStr.split(",") if inp]
+
+        for inp in inputList:
+          if inp > 6:
+            continue 
+          if inp < 1:
+            continue 
+
+        for inp in inputList:
+          player.cards[inp-1] = deck.deal()
+          player.cards[inp-1].showing = True
+
+        validInput = True
+      except:
+        print("Input Error: use commas to separated the cards you want to hold")
+
+    print(player.cards)
+    #Score
+    score = PokerScorer(player.cards)
+    straight = score.straight()
+    flush = score.flush()
+    highestCount = score.highestCount()
+    pairs = score.pairs()
+
+    # Royal flush
+    if straight and flush and straight == 14:
+      print("Royal Flush!!!")
+      print("+2000")
+      points += 2000
+
+    # Straight flush
+    elif straight and flush:
+      print("Straight Flush!")
+      print("+250")
+      points += 250
+
+    # 4 of a kind
+    elif score.fourKind():
+      print("Four of a kind!")
+      print("+125")
+      points += 125
+
+    # Full House
+    elif score.fullHouse():
+      print("Full House!")
+      print("+40")
+      points += 40
+
+    # Flush
+    elif flush:
+      print("Flush!")
+      print("+25")
+      points += 25
+
+    # Straight
+    elif straight:
+      print("Straight!")
+      print("+20")
+      points += 20
+
+    # 3 of a kind
+    elif highestCount == 3:
+      print("Three of a Kind!")
+      print("+15")
+      points += 15
+
+    # 2 pair
+    elif len(pairs) == 2:
+      print("Two Pairs!")
+      print("+10")
+      points += 10
+
+    # Jacks or better
+    elif pairs and pairs[0] > 10:
+      print ("Jacks or Better!")
+      print("+5")
+      points += 5
+
+    player.cards=[]
+
+    print()
+    print()
+    print()
